c7398e31ffde5a97bfb8205d2d2e279ece7705fc
Akif Islam <iamakifislam@gmail.com>
2022-04-06 02:21:25 +0600

    usages of req.params to determine specific chocolate from a list

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
 // Step 1.3 : write ",start" : "nodemon index.js" at package.json
 // Step 3.1 : Install body-parser to read data from Postman | npm install body-parser
 
+const chocolates = ["Snekers", "Kitkat", "Dairy Milk Silk"];
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 
+// For localhost:3000/ will send all chocolates list
 app.get("/", (req, res) => {
-  res.send("Sending you all the chocolates");
+  res.send(chocolates);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Adding : " + req.body.name);
+// For localhost:3000/2 will send Kitkat
+// For localhost:3000/3 will send Dairy Milk Silk
+
+app.get("/:chocolateID", (req, res) => {
+  const index = req.params.chocolateID;
+  res.send(chocolates[index]);
 });
 
 app.listen(3000);
