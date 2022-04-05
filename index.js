b3deb29488c9ae16c8c8df67dbdba7ae93a8ca96
Akif Islam <iamakifislam@gmail.com>
2022-04-06 05:11:25 +0600

    connected mongoDB with GET POST demo

--- index.js
+++ index.js
@@ -5,13 +5,29 @@
 // Step 9.1 : Simply, Start MongoDB Server on your PC (maybe using GitBash)
 // Step 9.2 : Install mongoose by typing ' npm install mongoose '
 
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
+const Book = require("./models/bookScheme");
+const bodyparser = require("body-parser");
+
 const app = express();
+app.use(bodyparser.json());
 
 //MongoDB Connection
 mongoose.connect("mongodb://localhost:27017/chocolates").then(() => {
   console.log("Database Connected Successfully");
+
+  app.get("/", async (req, res) => {
+    const books = await Book.find();
+    res.send(books);
+  });
+
+  app.post("/", async (req, res) => {
+    //Adding a new book to database
+    const book = new Book(req.body);
+    await book.save();
+    res.send("New book added to Database : " + book);
+  });
 });
 
 app.listen(3000);
