47ab4171cd3dd9f4487ef75c05a4353b95806b4d
Akif Islam <iamakifislam@gmail.com>
2022-04-06 02:14:55 +0600

    GET & Post first demo

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Step 1.1 : Install Nodemon for Live Server Feature : npm install nodemon
 // Step 1.2 : Install Express for Less Messy Code : npm install express
 // Step 1.3 : write ",start" : "nodemon index.js" at package.json
+// Step 3.1 : Install body-parser to read data from Postman | npm install body-parser
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Good People !");
+  res.send("Sending you all the chocolates");
 });
 
-app.get("/:name", (req, res) => {
-  res.send("Hello " + req.params.name);
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Adding : " + req.body.name);
 });
 
 app.listen(3000);
