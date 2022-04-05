8f61601370e66b865e8272683f2391af3e950b1e
Akif Islam <iamakifislam@gmail.com>
2022-04-06 02:00:29 +0600

    dynamic name calling

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Good People !");
+});
+
+app.get("/:name", (req, res) => {
+  res.send("Hello " + req.params.name);
 });
 
 app.listen(3000);
