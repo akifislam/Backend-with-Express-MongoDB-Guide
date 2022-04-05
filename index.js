321f31a5c52957a069bb38fde0ea3dc60abb8a9f
Akif Islam <iamakifislam@gmail.com>
2022-04-06 03:18:21 +0600

    create seperate file for routes

--- index.js
+++ index.js
@@ -7,38 +7,12 @@ var chocolates = ["Snekers", "Kitkat", "Dairy Milk Silk"];
 
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send(chocolates);
-});
+const chocolateRoutes = require("./routes/chocolates");
 
-app.get("/:chocolateID", (req, res) => {
-  const index = req.params.chocolateID;
-  res.send(chocolates[index]);
-});
-
-app.post("/", (req, res) => {
-  chocolates.push(req.body.name);
-  console.log("New Chocolate Added : " + req.body.name);
-  res.send("New Chocolate Added : " + req.body.name);
-});
-
-
-app.delete("/", (req, res) => {
-  chocolates = []; //Empty array;
-  console.log("Deleted All Chocolates");
-  res.send("Deleted all the chocolates successfully");
-});
-
-
-app.delete("/:id", (req, res) => {
-  const index = req.params.id;
-  chocolates.splice(index, 1); // Index theke 1 ta element delete korbe
-  console.log("Deleted 1 chocolate");
-  res.send("Deleted 1 chocolate successfully");
-});
+app.use("/chocolates", chocolateRoutes);
 
 app.listen(3000);
