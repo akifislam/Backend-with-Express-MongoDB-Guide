b650e287d1a6c427b2259947595c8fc981cf7f40
Akif Islam <iamakifislam@gmail.com>
2022-04-06 02:58:08 +0600

    GET POST DELETE with chocolates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Step 1.3 : write ",start" : "nodemon index.js" at package.json
 // Step 3.1 : Install body-parser to read data from Postman | npm install body-parser
 
-const chocolates = ["Snekers", "Kitkat", "Dairy Milk Silk"];
+var chocolates = ["Snekers", "Kitkat", "Dairy Milk Silk"];
 
 const express = require("express");
 const app = express();
@@ -24,6 +24,21 @@ app.post("/", (req, res) => {
   chocolates.push(req.body.name);
   console.log("New Chocolate Added : " + req.body.name);
   res.send("New Chocolate Added : " + req.body.name);
+});
+
+
+app.delete("/", (req, res) => {
+  chocolates = []; //Empty array;
+  console.log("Deleted All Chocolates");
+  res.send("Deleted all the chocolates successfully");
+});
+
+
+app.delete("/:id", (req, res) => {
+  const index = req.params.id;
+  chocolates.splice(index, 1); // Index theke 1 ta element delete korbe
+  console.log("Deleted 1 chocolate");
+  res.send("Deleted 1 chocolate successfully");
 });
 
 app.listen(3000);
