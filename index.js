a83f73b53b9321524085364f66f01c7266c0930b
Akif Islam <iamakifislam@gmail.com>
2022-04-15 11:31:57 +0600

    DELETE PUT with MongoDB

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // Step 3.1 : Install body-parser to read data from Postman | npm install body-parser
 // Step 9.1 : Simply, Start MongoDB Server on your PC (maybe using GitBash)
 // Step 9.2 : Install mongoose by typing ' npm install mongoose '
-//
+
+// What's New ? : Here we will learn to GET, POST, DELETE, PUT with MongoDB
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -15,11 +16,16 @@ const app = express();
 app.use(bodyparser.json());
 
 //MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/chocolates").then(() => {
+mongoose.connect("mongodb://localhost:27017/AkifBookShop").then(() => {
   console.log("Database Connected Successfully");
 
   app.get("/", async (req, res) => {
     const books = await Book.find();
+    res.send(books);
+  });
+
+  app.get("/:id", async (req, res) => {
+    const books = await Book.findById(req.params.id);
     res.send(books);
   });
 
@@ -29,6 +35,22 @@ mongoose.connect("mongodb://localhost:27017/chocolates").then(() => {
     await book.save();
     res.send("New book added to Database : " + book);
   });
-});
 
-app.listen(3000);
+  // To delete all documents in a collection, pass an empty document ( {} )
+  app.delete("/", async (req, res) => {
+    await Book.remove({});
+    res.send("Database Deleted Successfully");
+  });
+
+  app.delete("/:id", async (req, res) => {
+    await Book.findByIdAndRemove(req.params.id);
+    res.send("Book Deleted");
+  });
+
+  app.put("/:id", async (req, res) => {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body);
+    res.send("Book Updated : " + book);
+  });
+
+  app.listen(3000);
+});
